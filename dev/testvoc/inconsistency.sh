be9952f1d69a4dd14a2a9429dcc19ab0ecb98e12
Francis M. Tyers <ftyers@prompsit.com>
2013-05-22 21:53:42 +0000

    updates

--- dev/testvoc/inconsistency.sh
+++ dev/testvoc/inconsistency.sh
@@ -4,24 +4,24 @@ DIR=$1
 
 if [[ $DIR = "ukr-rus" ]]; then
 
-lt-expand ../../apertium-ru-uk.uk.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
+lt-expand ../../apertium-rus-ukr.ukr.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
         apertium-pretransfer|
-	lt-proc -b ../../uk-ru.autobil.bin |  tee $TMPDIR/$DIR.tmp_testvoc2.txt |
-        apertium-transfer -b ../../apertium-ru-uk.uk-ru.t1x  ../../uk-ru.t1x.bin |
-        apertium-interchunk ../../apertium-ru-uk.uk-ru.t2x  ../../uk-ru.t2x.bin |
-        apertium-postchunk ../../apertium-ru-uk.uk-ru.t3x  ../../uk-ru.t3x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
-        lt-proc -d ../../uk-ru.autogen.bin > $TMPDIR/$DIR.tmp_testvoc4.txt
+	lt-proc -b ../../ukr-rus.autobil.bin |  tee $TMPDIR/$DIR.tmp_testvoc2.txt |
+        apertium-transfer -b ../../apertium-rus-ukr.ukr-rus.t1x  ../../ukr-rus.t1x.bin |
+        apertium-interchunk ../../apertium-rus-ukr.ukr-rus.t2x  ../../ukr-rus.t2x.bin |
+        apertium-postchunk ../../apertium-rus-ukr.ukr-rus.t3x  ../../ukr-rus.t3x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
+        lt-proc -d ../../ukr-rus.autogen.bin > $TMPDIR/$DIR.tmp_testvoc4.txt
 paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
 
 elif [[ $DIR = "rus-ukr" ]]; then
 
-lt-expand ../../apertium-ru-uk.ru.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
+lt-expand ../../apertium-rus-ukr.rus.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
         apertium-pretransfer|
-	lt-proc -b ../../ru-uk.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |
-        apertium-transfer -b ../../apertium-ru-uk.ru-uk.t1x  ../../ru-uk.t1x.bin |
-        apertium-interchunk ../../apertium-ru-uk.ru-uk.t2x  ../../ru-uk.t2x.bin |
-        apertium-postchunk ../../apertium-ru-uk.ru-uk.t3x  ../../ru-uk.t3x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
-        lt-proc -d ../../ru-uk.autogen.bin > $TMPDIR/$DIR.tmp_testvoc4.txt
+	lt-proc -b ../../rus-ukr.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |
+        apertium-transfer -b ../../apertium-rus-ukr.rus-ukr.t1x  ../../rus-ukr.t1x.bin |
+        apertium-interchunk ../../apertium-rus-ukr.rus-ukr.t2x  ../../rus-ukr.t2x.bin |
+        apertium-postchunk ../../apertium-rus-ukr.rus-ukr.t3x  ../../rus-ukr.t3x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
+        lt-proc -d ../../rus-ukr.autogen.bin > $TMPDIR/$DIR.tmp_testvoc4.txt
 paste -d _ $TMPDIR/$DIR.tmp_testvoc1.txt $TMPDIR/$DIR.tmp_testvoc2.txt $TMPDIR/$DIR.tmp_testvoc3.txt $TMPDIR/$DIR.tmp_testvoc4.txt| sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
 
 
