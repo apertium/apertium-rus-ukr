83531611a875fe54470212091ef5f1ed35060a1e
Francis M. Tyers <ftyers@prompsit.com>
2013-07-03 23:21:45 +0000

    small change to scripts

--- dev/testvoc/inconsistency.sh
+++ dev/testvoc/inconsistency.sh
@@ -7,6 +7,7 @@ if [[ $DIR = "ukr-rus" ]]; then
 lt-expand ../../apertium-rus-ukr.ukr.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
         apertium-pretransfer|
 	lt-proc -b ../../ukr-rus.autobil.bin |  tee $TMPDIR/$DIR.tmp_testvoc2.txt |
+        lrx-proc ../../ukr-rus.autolex.bin |
         apertium-transfer -b ../../apertium-rus-ukr.ukr-rus.t1x  ../../ukr-rus.t1x.bin |
         apertium-interchunk ../../apertium-rus-ukr.ukr-rus.t2x  ../../ukr-rus.t2x.bin |
         apertium-postchunk ../../apertium-rus-ukr.ukr-rus.t3x  ../../ukr-rus.t3x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
@@ -18,6 +19,7 @@ elif [[ $DIR = "rus-ukr" ]]; then
 lt-expand ../../apertium-rus-ukr.rus.dix | grep -v '<prn><enc>' | grep -v 'REGEX' | grep -v ':<:' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/$DIR.tmp_testvoc1.txt |
         apertium-pretransfer|
 	lt-proc -b ../../rus-ukr.autobil.bin | tee $TMPDIR/$DIR.tmp_testvoc2.txt |
+        lrx-proc ../../rus-ukr.autolex.bin |
         apertium-transfer -b ../../apertium-rus-ukr.rus-ukr.t1x  ../../rus-ukr.t1x.bin |
         apertium-interchunk ../../apertium-rus-ukr.rus-ukr.t2x  ../../rus-ukr.t2x.bin |
         apertium-postchunk ../../apertium-rus-ukr.rus-ukr.t3x  ../../rus-ukr.t3x.bin  | tee $TMPDIR/$DIR.tmp_testvoc3.txt |
